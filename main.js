274718bf55c20337dc453f55bf635c25e9de476a
vioool <violapuhalovic@gmail.com>
2019-05-02 17:15:06 +0200

    update files

--- main.js
+++ main.js
@@ -9,37 +9,33 @@
 //document.getElementById("senate-data").innerHTML = JSON.stringify(data.results[0].members[1].last_name, null, 2); //gives back the last name of the second member of the results of the data object of the document and append to the HTMLdoc
 
 var allMembers = data.results[0].members; //make new object (where multiple value is stored) you can use in your equation!
-//.filter(Boolean) // the identity function: `item => item` would also work here :)
-//.map(makeDisplayName);
-console.log(allMembers)
-//var text = "";
-//for (i = 0; i < allMembers.length; i++) {
-//    text += allMembers[i].first_name + "<br>";
-//}
-//
-//document.getElementById("senate-data").innerHTML = JSON.stringify(text, null, 2);
 
+console.log(allMembers) //log to console to see if its there
 
 buildTable()
 
 function buildTable() {
-    var table = document.createElement("Table"); // Create a table 
+    //    var table = document.createElement("Table"); // Create a table 
 
     var tblB = document.createElement("TBody"); // Create a table body 
-    table.appendChild(tblB); // Append/attach the tablebody to the table.
+    // Append/attach the tablebody to the table.
     for (var i = 0; i < allMembers.length; i++) { // Loop to create cells and rows&run through the array outside object/doc
-        //        if (!middle_name == null) {}
 
         var tr = document.createElement('tr'); // Create the rows
         var name; // Create a new object to specify nulls in names
         if (allMembers[i].middle_name != null) {
-            name = allMembers[i].last_name + ', ' + allMembers[i].middle_name + ', ' + allMembers[i].first_name;
+            name = (allMembers[i].last_name + ', ' + allMembers[i].middle_name + ', ' + allMembers[i].first_name).link(allMembers[i].url);
         } else {
-            name = allMembers[i].last_name + ', ' + allMembers[i].first_name;
+            name = (allMembers[i].last_name + ', ' + allMembers[i].first_name).link(allMembers[i].url);
         } // create statement to avoiding nulls before!!! the creation of the textnode => see next line.
-        var textnodeName = document.createTextNode(name); // Create a text node
-        tr.appendChild(textnodeName); // Append/attach the text to <tr>
-        tblB.appendChild(tr); // Append/attach table-row to the tablebody
+        var tdName = document.createElement('td');
+        tdName.innerHTML = name;
+        
+//        var textnodeName = document.createTextNode(name); // Create a text node
+//        tdName.appendChild(textnodeName);
+        tr.appendChild(tdName);
+        // Append/attach the text to <tr>
+        // Append/attach table-row to the tablebody
 
         var td = document.createElement('td'); // Create the cells
         var textnodeParty = document.createTextNode(allMembers[i].party); // Create a text knooppunt
@@ -60,11 +56,21 @@ function buildTable() {
         var textnodeVotes = document.createTextNode(allMembers[i].votes_with_party_pct); // Create a text knooppunt
         td.appendChild(textnodeVotes); // Append/attach the text to <td>
         tr.appendChild(td); // Append/attach cells to the rows
+        tblB.appendChild(tr);
 
     }
 
     console.log(table)
-    document.getElementById("table").appendChild(table);
+    document.getElementById("table").appendChild(tblB);
 
 
+}
+
+//addLink();
+
+function addLink() {
+
+
+//    var x = document.getElementById("myLink").href; // 
+//    document.getElementById("link").innerHTML = x;
 }
