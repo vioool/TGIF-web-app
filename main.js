3f214fe97a85bc9e34a9d0e912cbca203a87a7f8
vioool <violapuhalovic@gmail.com>
2019-05-04 14:25:08 +0200

    checkbox working

--- main.js
+++ main.js
@@ -12,61 +12,75 @@ var allMembers = data.results[0].members; //make new object (where multiple valu
 
 var republican = document.getElementById("inlineCheckbox1");
 republican.addEventListener("click", function () {
-        filterParty();
-    });
+    filterParty();
+});
 
-    var republican = document.getElementById("inlineCheckbox2"); republican.addEventListener("click", function () {
-            filterParty();
-        });
+var democrat = document.getElementById("inlineCheckbox2");
+democrat.addEventListener("click", function () {
+    filterParty();
+});
 
-        var republican = document.getElementById("inlineCheckbox3"); republican.addEventListener("click", function () {
-                filterParty();
-            });
+var independent = document.getElementById("inlineCheckbox3");
+independent.addEventListener("click", function () {
+    filterParty();
+});
 
-            console.log(filteredArray)
+//console.log(filteredArray)
 
 
-            filterParty()
+//filterParty()
 
-            function filterParty() { //create filter
-                var filteredArray = []; //?????????????
+buildTable(allMembers)
 
-                for (var i = 0; i < allMembers.length; i++) {
-                    if (allMembers[i].party == "R" && republican.checked == true) {
-                        filteredArray.push(allMembers[i]);
-                    }
+function filterParty() { //create filter
+    var filteredArray = []; //?????????????
 
-            }
-            if (allMembers[i].party == "D" && republican.checked == true) {
-                filteredArray.push(allMembers[i]);
-            }
+    for (var i = 0; i < allMembers.length; i++) {
+        if (allMembers[i].party == "R" && republican.checked == true) {
+            filteredArray.push(allMembers[i]);
+        }
+
+
+        if (allMembers[i].party == "D" && democrat.checked == true) {
+            filteredArray.push(allMembers[i]);
+        }
+
+
+        if (allMembers[i].party == "I" && independent.checked == true) {
+            filteredArray.push(allMembers[i]);
+        }
+
+     
 
     }
-    if (allMembers[i].party == "I" && republican.checked == true) {
-        filteredArray.push(allMembers[i]);
-    }
+       buildTable(filteredArray)
+}
 
 
 
-document.getElementById("senate-data").innerHTML = (filteredArray);
+//    document.getElementById("senate-data").innerHTML = (filteredArray);
 
 
 
-buildTable()
+//buildTable(allMembers)
 
-function buildTable() {
+function buildTable(array) {
+    var table = document.getElementById("table"); //Get Table Node from HTML 
     //    var table = document.createElement("Table"); // Create a table 
+    table.innerHTML = "";
 
-    var tblB = document.createElement("TBody"); // Create a table body 
+    var tblB = document.createElement("TBody");
+
+    // Create a table body 
     // Append/attach the tablebody to the table.
-    for (var i = 0; i < allMembers.length; i++) { // Loop to create cells and rows&run through the array outside object/doc
+    for (var i = 0; i < array.length; i++) { // Loop to create cells and rows&run through the array outside object/doc
 
         var tr = document.createElement('tr'); // Create the rows
         var name; // Create a new object to specify nulls in names
-        if (allMembers[i].middle_name != null) {
-            name = (allMembers[i].last_name + ', ' + allMembers[i].middle_name + ', ' + allMembers[i].first_name).link(allMembers[i].url);
+        if (array[i].middle_name != null) {
+            name = (array[i].last_name + ', ' + array[i].middle_name + ', ' + array[i].first_name).link(array[i].url);
         } else {
-            name = (allMembers[i].last_name + ', ' + allMembers[i].first_name).link(allMembers[i].url);
+            name = (array[i].last_name + ', ' + array[i].first_name).link(array[i].url);
         } // create statement to avoiding nulls before!!! the creation of the textnode => see next line.
         var tdName = document.createElement('td');
         tdName.innerHTML = name;
@@ -78,39 +92,32 @@ function buildTable() {
         // Append/attach table-row to the tablebody
 
         var td = document.createElement('td'); // Create the cells
-        var textnodeParty = document.createTextNode(allMembers[i].party); // Create a text knooppunt
+        var textnodeParty = document.createTextNode(array[i].party); // Create a text knooppunt
         td.appendChild(textnodeParty); // Append/attach the text to <td>
         tr.appendChild(td); // Append/attach cells to the rows
 
         var td = document.createElement('td'); // Create the cells
-        var textnodeState = document.createTextNode(allMembers[i].state); // Create a text knooppunt
+        var textnodeState = document.createTextNode(array[i].state); // Create a text knooppunt
         td.appendChild(textnodeState); // Append/attach the text to <td>
         tr.appendChild(td); // Append/attach cells to the rows
 
         var td = document.createElement('td'); // Create the cells
-        var textnodeSeniority = document.createTextNode(allMembers[i].seniority); // Create a text knooppunt
+        var textnodeSeniority = document.createTextNode(array[i].seniority); // Create a text knooppunt
         td.appendChild(textnodeSeniority); // Append/attach the text to <td>
         tr.appendChild(td); // Append/attach cells to the rows
 
         var td = document.createElement('td'); // Create the cells
-        var textnodeVotes = document.createTextNode(allMembers[i].votes_with_party_pct); // Create a text knooppunt
+        var textnodeVotes = document.createTextNode(array[i].votes_with_party_pct); // Create a text knooppunt
         td.appendChild(textnodeVotes); // Append/attach the text to <td>
         tr.appendChild(td); // Append/attach cells to the rows
         tblB.appendChild(tr);
 
     }
 
-    console.log(table)
-    document.getElementById("table").appendChild(tblB);
+//    console.log(table)
+    table.appendChild(tblB);
 
 
 }
 
 //addLink();
-
-function addLink() {
-
-
-    //    var x = document.getElementById("myLink").href; // 
-    //    document.getElementById("link").innerHTML = x;
-}
